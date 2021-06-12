ee87a0939ee03ef94ec6f6d5211ea51f1c1c8210
Parajulib <b4parajuli@gmail.com>
2021-06-12 13:07:13 -0500

    Using div instead of table

--- code.js
+++ code.js
@@ -1,25 +1,27 @@
 let irlandFlag = [
-  [1, 1, 1, 2, 2, 2, 3, 3, 3],
-  [1, 1, 1, 2, 2, 2, 3, 3, 3],
-  [1, 1, 1, 2, 2, 2, 3, 3, 3],
-  [1, 1, 1, 2, 2, 2, 3, 3, 3],
-  [1, 1, 1, 2, 2, 2, 3, 3, 3],
+  [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3],
+  [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3],
+  [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3],
+  [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3],
+  [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3],
+  [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3],
+  [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3],
 ];
 
 function displayFlag(flagArray) {
   //1.create a table element
-  let flagTable = document.createElement("table");
-  document.body.append(flagTable);
+  let flagDiv = document.createElement("div");
+  document.body.append(flagDiv);
   //2 loop from zero to length of flagArray
   for (let rowNum = 0; rowNum < flagArray.length; rowNum++) {
     let rowArray = flagArray[rowNum];
     //create new tr element
-    let rowEl = document.createElement("tr");
-    flagTable.append(rowEl);
+    let rowEl = document.createElement("ul");
+    flagDiv.append(rowEl);
     //3 loop from zero to rowArray length
     for (let cellNum = 0; cellNum < rowArray.length; cellNum++) {
       // create new td and append to tr
-      let colorPixel = document.createElement("td");
+      let colorPixel = document.createElement("li");
       colorPixel.classList.add("color" + rowArray[cellNum]);
       rowEl.append(colorPixel);
     }
